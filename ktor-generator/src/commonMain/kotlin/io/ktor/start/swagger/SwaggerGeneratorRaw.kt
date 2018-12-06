10df48dd37f6fd66b448d1f70611da7f6b871ddc
Markus Schweizer <markus.schweizer@ffuf.de>
2018-12-06 16:20:09 +0100

    went back to strings with lists..

--- ktor-generator/src/commonMain/kotlin/io/ktor/start/swagger/SwaggerGeneratorRaw.kt
+++ ktor-generator/src/commonMain/kotlin/io/ktor/start/swagger/SwaggerGeneratorRaw.kt
@@ -166,7 +166,12 @@ object SwaggerGeneratorRaw : SwaggerGeneratorBase() {
 
                     for (route in model.routes.values) {
                         for (method in route.methodsList) {
-                            val responseType = method.responseType.toKotlinType()
+                            var responseType = method.responseType.toKotlinType()
+                            val resultType = responseType
+                            val isListType = isListType(responseType)
+                            if (isListType) {
+                                responseType = "String"
+                            }
                             SEPARATOR {
                                 doc(
                                     title = "",
@@ -181,7 +186,7 @@ object SwaggerGeneratorRaw : SwaggerGeneratorBase() {
                                         val default = if (param.required) "" else "? = null"
                                         +"${param.name}: ${param.schema.toKotlinType()}$default, // ${param.inside}"
                                     }
-                                    +"callback: (result: $responseType?, error: Throwable?) -> Unit"
+                                    +"callback: (result: $resultType?, error: Throwable?) -> Unit"
                                 }
                                 +") " {
                                     val replacedPath = method.path.replace(Regex("\\{(\\w+)\\}")) {
@@ -205,7 +210,12 @@ object SwaggerGeneratorRaw : SwaggerGeneratorBase() {
                                                 +"this.body = serializerKotlin.write(${method.parametersBody[0].name})"
                                             }
                                         }
-                                        +"callback(result, null)"
+                                        if (isListType) {
+                                            +"val listResult = JSON(strictMode = false).parse(${getListType(method.responseType.toKotlinType())}.serializer().list, result)"
+                                            +"callback(listResult, null)"
+                                        } else {
+                                            +"callback(result, null)"
+                                        }
                                     }
                                     + ")"
                                 }
