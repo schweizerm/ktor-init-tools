d99bc8ccc57d7bc5997ba015af0fed65f740f825
Markus Schweizer <markus.schweizer@ffuf.de>
2019-01-30 14:09:30 +0100

    strings don't get wrapped in quotationmarks anymore

--- ktor-generator/src/commonMain/kotlin/io/ktor/start/swagger/SwaggerGeneratorRaw.kt
+++ ktor-generator/src/commonMain/kotlin/io/ktor/start/swagger/SwaggerGeneratorRaw.kt
@@ -212,7 +212,8 @@ object SwaggerGeneratorRaw : SwaggerGeneratorBase() {
                                                                 +"${param.name}?.let { this.append(${param.name.quote()}, it.joinToString(\",\")) }"
                                                             }
                                                             else {
-                                                                +"${param.name}?.let { this.append(${param.name.quote()}, \"\$it\") }"
+                                                                val appendValue = if (param.schema.toKotlinType() == "String") "it" else "\"\$it\""
+                                                                +"${param.name}?.let { this.append(${param.name.quote()}, $appendValue) }"
                                                             }
                                                         }
                                                     }
